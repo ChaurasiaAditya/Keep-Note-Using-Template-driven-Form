cdc9e0a037f1c6a876de536e886a3834279b128a
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-11-08 10:23:36 +0530

    Added minimum Date

--- keep-note/src/app/add-note/add-note.component.ts
+++ keep-note/src/app/add-note/add-note.component.ts
@@ -10,26 +10,30 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class AddNoteComponent implements OnInit {
 
-  NOTE?:Note[] = [];
-
   note:Note = {}
 
   panelOpenState:boolean = false;
+
+  // ForMinimum Date
+  currentdate: Date = new Date();
+  minimumdate:Date = new Date(this.currentdate.getFullYear(),this.currentdate.getMonth(),this.currentdate.getDate());
+
+  // For category
   categories:string[] = ["office","Review"]
   
   addNote(){
     this.noteservice.addNote(this.note).subscribe(
-      {next:() => this._snackBar.open('Note Added successfully', 'dismis', {​
+      {next:() => {this._snackBar.open('Note Added successfully', 'dismis', {​
         duration: 5000,​
         panelClass: ['mat-toolbar', 'mat-primary']​
-        })}
+        })}}
     )
   }
   
   constructor(private noteservice:NoteService,private _snackBar: MatSnackBar) { }
 
+
   ngOnInit(): void {
-    this.noteservice.getNotes().subscribe(data => this.NOTE = data)
   }
 
 }
